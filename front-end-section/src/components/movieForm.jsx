ff629d50d7bda9bd1a0f3fa589446d9d33a4f7e2
ebenizerk <abenezerk@zeraftech.com>
2022-06-29 12:48:42 +0300

    Chnage checked postion to the top

--- front-end-section/src/components/movieForm.jsx
+++ front-end-section/src/components/movieForm.jsx
@@ -9,8 +9,9 @@ class MovieForm extends Login {
     data: {
       title: "",
       genreId: "",
-      bought: "",
-      numberInStock: "",
+      bought: false,
+      price: 0,
+      numberInStock: 0,
       dailyRentalRate: "",
     },
     geners: [],
@@ -21,18 +22,19 @@ class MovieForm extends Login {
     title: joi.string().required().label("Title"),
     genreId: joi.string().required().label("Genres"),
     numberInStock: joi.required().label("NumberInStock"),
-    dailyRentalRate: joi
-      .string()
-      .required()
-      .min(0)
-      .max(10)
-      .label("dailyRentalRate"),
+    // dailyRentalRate: joi
+    //   .string()
+    //   .required()
+    //   .min(0)
+    //   .max(10)
+    //   .label("dailyRentalRate"),
   };
 
   async populateGenres() {
     const { data: geners } = await getGenres();
     this.setState({ geners });
   }
+
   async populateMovies() {
     try {
       const movieId = this.props.match.params.id;
@@ -54,6 +56,8 @@ class MovieForm extends Login {
     return {
       _id: movie._id,
       title: movie.title,
+      bought: movie.bought,
+      price: movie.price,
       genreId: movie.genre._id,
       numberInStock: movie.numberInStock,
       dailyRentalRate: movie.dailyRentalRate,
@@ -73,9 +77,11 @@ class MovieForm extends Login {
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("title", "Title")}
           {this.renderSelect("genreId", "Genere", this.state.geners)}
-          {this.renderChecked("bought", "Is Bought", this.state.geners)}
           {this.renderInput("numberInStock", "Number in Stock", "number")}
           {this.renderInput("dailyRentalRate", "Rate")}
+          {this.props.match.params.id != "new"
+            ? this.renderChecked("bought", "Is Bought", this.state.geners)
+            : ""}
           {this.renderButton("Save")}
         </form>
       </div>
